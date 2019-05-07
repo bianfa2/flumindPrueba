0ea89a305051d6af097c785560a7d5c04bdc9a52
Fabian <fabianserna2013@gmail.com>
2019-05-06 23:22:37 -0500

    principal completo

--- js/mainPage.js
+++ js/mainPage.js
@@ -3,21 +3,14 @@ function showMainElements() {
     var abstracts = document.getElementsByClassName("abstract");
     var elements = document.getElementsByClassName("main_element");
 
-    abstracts[0].className = "abstract abstract1 animated fadeIn";
+    abstracts[0].className = "abstract animated fadeIn";
 
     setTimeout(function(){
-        abstracts[1].className = "abstract abstract2 animated fadeIn";
-    }, 500);
-
-    setTimeout(function(){
-        abstracts[2].className = "abstract abstract3 animated fadeIn";
+        document.getElementById("tabs").className = "tabs animated fadeIn";
+        document.getElementById("buttonMenu").className = "menu_icon animated fadeIn";
+        elements[0].className = "main_element logo animated fadeIn";
+        elements[1].className = "main_element main_title animated bounceInDown";
+        elements[2].className = "main_element main_desc desc animated bounceInDown";
+        elements[3].className = "main_element options animated bounceInDown";    
     }, 1000);
-
-    setTimeout(function(){
-        document.getElementsByClassName("tabs")[0].className = "tabs animated fadeIn";
-        elements[0].className = "main_element main_title animated bounceInDown";
-        elements[1].className = "main_element main_desc animated bounceInDown";
-        elements[2].className = "main_element options animated bounceInDown";        
-    }, 1500);
-
 }
--- js/navegation.js
+++ js/navegation.js
@@ -1,23 +1,49 @@
 var pages;
 var tabs;
+var menu;
+var menuIcon;
+var closeIcon;
+var pageOpaque;
 
 function start() {
-    initVariable();       
-    pages[1].className = "page animated fadeIn";
-    loadMainPage();    
+    initVariable();  
+    loadMainPage();     
+    assingEvents();        
 }
 
 function initVariable() {
 
     pages = document.getElementsByClassName("page");
+    menu = document.getElementById("menu");
+    menuIcon = document.getElementById("buttonMenu");
+    closeIcon = document.getElementById("closeIcon");
+    pageOpaque = document.getElementById("opaque");
 
 }
 
+function assingEvents(){
+    menuIcon.addEventListener("click", openMenu);
+    closeIcon.addEventListener("click", closeMenu);
+}
+
 function loadMainPage() {
+    pages[1].className = "page animated fadeIn";
     setTimeout(function(){
 
         pages[0].className = "page hidden";
         showMainElements();
 
     }, 1000);
+}
+
+function openMenu(){
+    pageOpaque.className = "page opaque";
+    pageOpaque.style.opacity = ".4";  
+    menu.style.transform = "translateX(-100%)";
+}
+
+function closeMenu(){
+    pageOpaque.className = "hidden";
+    pageOpaque.style.opacity = "0";  
+    menu.style.transform = "translateX(100%)";
 }
